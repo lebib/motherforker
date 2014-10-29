5f2a30ccd2eb3280ec23c4535c58ce63c9eeba4b
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-10-29 06:08:31 +0100

    Fix admin file

--- lacave/admin.py
+++ lacave/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Location, Type, Item
 
 
-admin.register(Location)
-admin.register(Type)
-admin.register(Item)
+admin.site.register(Location)
+admin.site.register(Type)
+admin.site.register(Item)
